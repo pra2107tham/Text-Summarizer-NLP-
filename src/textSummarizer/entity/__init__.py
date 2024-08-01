acd0199a3dd0f4349386e9fd40c5d6a7c4e7c7bc
Pratham Shirbhate <prathamshirbhate1909@gmail.com>
2024-08-01 16:24:26 +0530

    chore: Update PyTorch version to 2.4.0 in model trainer configuration and also done model evaluation

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -44,3 +44,12 @@ class ModelTrainerConfig:
     gradient_accumulation_steps: int
     
     
+from dataclasses import dataclass
+from pathlib import Path
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path

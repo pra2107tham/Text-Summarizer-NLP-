a422fa95bb58ecda6fb02f8ca9101c38d563fdc6
Pratham Shirbhate <prathamshirbhate1909@gmail.com>
2024-08-01 15:52:36 +0530

    chore: Add data transformation functionality to training pipeline

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
 from pathlib import Path
-
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
 
 from dataclasses import dataclass
 from pathlib import Path
@@ -15,3 +15,12 @@ class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
+    
+    
+from dataclasses import dataclass
+from pathlib import Path
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
